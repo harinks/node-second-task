f86239e17823396bf9bcc1b479ec22e5ac5ac3ab
nkshariharan <nkshariharan17@gmail.com>
2022-01-10 09:20:41 +0530

    second commit

--- index.js
+++ index.js
@@ -6,23 +6,19 @@ const app = express();
 
 app.get("/", (req,res) =>{
 var date = new Date();
-var currentTime = date.getTime();
-var timestamp = currentTime.toString();
+var currentTime = date.getTime().toString();
 var dateAndTime = `${date.getDate()}-${(date.getMonth())}-${date.getFullYear()}-${date.getHours()}-${date.getMinutes()}`;
-var finalTime = (`${dateAndTime} ${".txt"}`);
-  fs.writeFile(finalTime, timestamp, function (err){
+var file = (`${dateAndTime}${".txt"}`);
+  fs.writeFile(`./files/${file}`, currentTime, function (err){
     if(err) throw err;
     console.log("file created")
-    res.sendFile(process.argv[2])
 })
 })
-
 
 app.get("/", (req,res) =>{
-    fs.readdir(process.argv[2], function(err, files) {
+    fs.readdir("./files", function(err, files) {
         if(err) throw err;
         res.json(files)
-        console.log(files)
     })
 })
 
